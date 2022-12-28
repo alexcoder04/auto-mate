4fe26a16466198499544f1748f2b3b7b85548d69
alexcoder04 <alexcoder04@protonmail.com>
2022-12-28 13:12:27 +0100

    config file, parallel sequences, clean up main

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"strings"
+	"sync"
 
 	"github.com/alexcoder04/auto-mate/actions"
 )
+
+type ConfigAction struct {
+	Type string         `json:"type"`
+	Args map[string]any `json:"args"`
+}
+
+var Actions = map[string]func(map[string]any) map[string]any{
+	"calendar-add":      actions.CalendarAdd,
+	"empty":             actions.Empty,
+	"file-open":         actions.FileOpen,
+	"folder-create":     actions.FolderCreate,
+	"notification":      actions.Notification,
+	"on.battery":        actions.OnBattery,
+	"on.battery-status": actions.OnBatteryStatus,
+	"on.file-changed":   actions.OnFileChanged,
+	"on.time":           actions.OnTime,
+	"on.wifi":           actions.OnWifi,
+	"qr-encode":         actions.QrEncode,
+	"stop":              actions.Stop,
+	"wifi":              actions.Wifi,
+}
 
 func m(b map[string]any, a map[string]any) map[string]any {
 	res := map[string]any{}
@@ -25,73 +50,43 @@ func m(b map[string]any, a map[string]any) map[string]any {
 	return res
 }
 
-func main() {
-	sequence := []string{
-		"on-time",
-		"on-wifi",
-		"qr-encode",
-		"file-open",
-		"notification",
-		"on-file-changed",
-		"notification",
-	}
-	args := []map[string]any{
-		{
-			"time":     "12:43",
-			"weekdays": "1101111",
-		},
-		{
-			"status": "enabled",
-		},
-		{
-			"data": "https://github.com/alexcoder04",
-		},
-		{},
-		{
-			"title": "QR code ready",
-			"body":  ":->file",
-		},
-		{
-			"file": "/home/alex/.bashrc",
-		},
-		{
-			"title": "bashrc changed",
-		},
-	}
-
+func handleSequence(sequence []ConfigAction, wg *sync.WaitGroup) {
 	out := map[string]any{}
-	for i, a := range sequence {
-		switch a {
-		case "on-battery":
-			out = actions.OnBattery(m(args[i], out))
-		case "on-battery-status":
-			out = actions.OnBatteryStatus(m(args[i], out))
-		case "calendar-add":
-			out = actions.CalendarAdd(m(args[i], out))
-		case "empty":
-			out = actions.Empty(m(args[i], out))
-		case "file-open":
-			out = actions.FileOpen(m(args[i], out))
-		case "on-file-changed":
-			out = actions.OnFileChanged(m(args[i], out))
-		case "folder-create":
-			out = actions.FolderCreate(m(args[i], out))
-		case "notification":
-			out = actions.Notification(m(args[i], out))
-		case "qr-encode":
-			out = actions.QrEncode(m(args[i], out))
-		case "stop":
-			out = actions.Stop(m(args[i], out))
-		case "on-time":
-			out = actions.OnTime(m(args[i], out))
-		case "wifi":
-			out = actions.Wifi(m(args[i], out))
-		case "on-wifi":
-			out = actions.OnWifi(m(args[i], out))
+	for _, a := range sequence {
+		if _, ok := Actions[a.Type]; !ok {
+			fmt.Printf("Action %s not found\n", a)
+			return
 		}
 
+		out = Actions[a.Type](m(a.Args, out))
+
 		if !out["success"].(bool) {
+			fmt.Printf("Action %s failed\n", a)
 			return
 		}
 	}
+	wg.Done()
+}
+
+func main() {
+	data, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Printf("Failed to read config file: %s\n", err.Error())
+		return
+	}
+
+	config := [][]ConfigAction{}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Printf("Failed to parse config file: %s\n", err.Error())
+		return
+	}
+
+	var wg sync.WaitGroup
+	for _, sequence := range config {
+		go handleSequence(sequence, &wg)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
